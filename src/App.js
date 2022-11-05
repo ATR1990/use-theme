ef03b44b09722110f251c67883e4221f5b7c40a2
AltynbekovT <tauyekelaltynbekov@gmail.com>
2022-11-05 17:18:52 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useTheme } from "./hooks/use-theme"
 
-function App() {
+export const App = () => {
+  const { theme, setTheme } = useTheme()
+
+  const onLightThemeClick = () => {
+    setTheme('light')
+  }
+
+  const onDarkThemeClick = () => {
+    setTheme('dark')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>useTheme</h1>
+      <button onClick={onLightThemeClick}>Light</button>
+      <button onClick={onDarkThemeClick}>Dark</button>
+    </>
   );
 }
-
-export default App;
